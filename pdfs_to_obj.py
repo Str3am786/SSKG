0b15b0750945bb3d6a77c6a2611f2c8f5a5b49dd
Str3am786 <arroyo.m786@gmail.com>
2023-07-28 11:34:20 +0200

    clean

--- pdfs_to_obj.py
+++
@@ -1,7 +0,0 @@
-from object_creator.downloaded_to_paperObj import dwnlddJson_to_paperJson
-from object_creator.pdf_to_downloaded import pdfs_to_downloaded_Json
-from object_creator.pipeline import from_papers_json_to_unidir
-
-path_json = pdfs_to_downloaded_Json("/home/kg/PDFs")
-paper_json = dwnlddJson_to_paperJson("../test/pdfs/pdf_metadata.json", "../test/pdfs")
-from_papers_json_to_unidir(paper_json,"/home/kg")
